465dff74c9f1117424fe5700443570c9bab5c5f7
anatol <anatolminor91@inbox.ru>
2021-05-20 19:48:19 +0300

    HW3

--- library/library/urls.py
+++ library/library/urls.py
@@ -1,29 +1,18 @@
-"""library URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from authapp.views import UserModelViewSet
+from todoapp.views import *
 
 
-router_user = DefaultRouter()
-router_user.register('user', UserModelViewSet)
+router = DefaultRouter()
+router.register('user', UserModelViewSet)
+router.register('todo', TodoModelViewSet)
+router.register('project', ProjectModelViewSet)
+router.register('project-users', ProjectUsersViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include(router_user.urls)),
+    path('api/', include(router.urls)),
     path('api-author/', include('rest_framework.urls')),
 ]
---
+++ library/todoapp/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework.serializers import ModelSerializer
+from todoapp.models import Project, Todo, ProjectUsers
+
+
+class ProjectModelSerializer(ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+class TodoModelSerializer(ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = '__all__'
+
+
+class ProjectUsersModelSerializer(ModelSerializer):
+    class Meta:
+        model = ProjectUsers
+        fields = '__all__'
